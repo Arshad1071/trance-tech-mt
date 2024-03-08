e260a978ba8f68087b24e67abe37a1969a535bcb
Arshad vp <Arshad1071@yahoo.in>
2024-03-08 13:57:50 +0530

    dashboard ui added

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,24 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./WeatherApp.css";
+import { Grid } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import WetherImg from "../img/weather-icon.webp";
 
 const Dashboard = () => {
   const [weatherData, setWeatherData] = useState(null);
   const [city, setCity] = useState(""); // Set default city here
+
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+    margin: 10,
+  }));
 
   useEffect(() => {
     const fetchData = async () => {
@@ -27,25 +41,83 @@ const Dashboard = () => {
   };
 
   return (
-    <div className="weather-app">
-      <h1>Weather App</h1>
-      <input
-        type="text"
-        placeholder="Enter city"
-        value={city}
-        onChange={handleCityChange}
-      />
-      {weatherData && (
-        <div className="weather-info">
-          <h2>
-            {weatherData.name}, {weatherData.sys.country}
-          </h2>
-          <p>{weatherData.weather[0].description}</p>
-          <p>Temperature: {weatherData.main.temp} °C</p>
-          <p>Humidity: {weatherData.main.humidity}%</p>
-        </div>
-      )}
-    </div>
+    <Grid
+      container
+      spacing={2}
+      sx={{ marginLeft: 2, marginRight: 2, marginTop: 2 }}
+    >
+      <Grid item xs={6} md={3}>
+        <input
+          style={{ width: "85%", margin: "10px" }}
+          type="text"
+          placeholder="Search for places"
+          value={city}
+          onChange={handleCityChange}
+        />
+        <Item>
+          <h1>Weather App</h1>
+
+          {weatherData && (
+            <div className="weather-info">
+              <img
+                src={`http://openweathermap.org/img/w/${weatherData?.weather[0]?.icon}.png`}
+                alt={WetherImg}
+              />
+              <h2>
+                {weatherData.name}, {weatherData.sys.country}
+              </h2>
+              <p>{weatherData.weather[0].description}</p>
+              <p>Temperature: {weatherData.main.temp} °C</p>
+              <p>Humidity: {weatherData.main.humidity}%</p>
+            </div>
+          )}
+        </Item>
+      </Grid>
+      <Grid item xs={6} md={8} sx={{  marginLeft: 5 }}>
+        <h4 style={{ marginBottom: "50px" }}>Today's Highlights</h4>
+        {weatherData && (
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid
+              container
+              spacing={{ xs: 2, md: 6 }}
+              columns={{ xs: 4, sm: 8, md: 12 }}
+            >
+              <Grid xs={2} sm={4} md={4}>
+                <Item>
+                  <p>UV index: {weatherData.main.humidity}%</p>
+                </Item>
+              </Grid>
+              <Grid xs={2} sm={4} md={4}>
+                <Item>
+                  <p>Wind Status: {weatherData.main.humidity}</p>
+                </Item>
+              </Grid>
+              <Grid xs={2} sm={4} md={4}>
+                <Item>xs=2</Item>
+              </Grid>
+              <Grid xs={2} sm={4} md={4}>
+                <Item>xs=2</Item>
+              </Grid>
+              <Grid xs={2} sm={4} md={4}>
+                <Item>xs=2</Item>
+              </Grid>
+              <Grid xs={2} sm={4} md={4}>
+                <Item>xs=2</Item>
+              </Grid>
+              <Grid xs={2} sm={4} md={4}>
+                <Item>xs=2</Item>
+              </Grid>
+              <Grid xs={2} sm={4} md={4}>
+                <Item>xs=2</Item>
+              </Grid>
+              <Grid xs={2} sm={4} md={4}>
+                <Item>xs=2</Item>
+              </Grid>
+            </Grid>
+          </Box>
+        )}
+      </Grid>
+    </Grid>
   );
 };
 
